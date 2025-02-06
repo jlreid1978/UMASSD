73f0aec07ec0e3193c3b03d674a0cb83a46b75cd
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2025-02-06 10:42:30 -0500

    Lab 4 complete

--- lab4.c
+++ lab4.c
@@ -8,25 +8,25 @@
 
 
 // Function to calculate adding num1 and num2
-long long Addition(double num1, double num2) {
+double Addition(double num1, double num2) {
     return num1 + num2;
 }
 
 
 // Function to calculate subtracting num2 from num1
-long long Subtraction(double num1, double num2) {
+double Subtraction(double num1, double num2) {
     return num1 - num2;
 }
 
 
 // Function to calculate multiplying num1 and num2
-long long Multiplication(double num1, double num2) {
+double Multiplication(double num1, double num2) {
     return num1 * num2;
 }
 
 
 // Function to calculate dividing num2 by num1
-long long Division(double num1, double num2) {
+double Division(double num1, double num2) {
     return num1 / num2;
 }
 
@@ -40,21 +40,29 @@ int main(void){
     
 
     //Ask user for their selection
-    printf("Please select the number of whatever function you wish to use?\n"
+    printf("Please select the number of whatever function you wish to use.\n"
     "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
     scanf("%d", &selection);
 
     //Validation for selection. If selection is valid, ask for two numbers.
     if ((selection >= 1) && (selection <= 4)){
         printf("\nPlease enter number 1: ");
-        scanf("%lf", &num1);
-
+        //Validate taht a number was entered
+        if (scanf("%lf", &num1) != 1){
+            printf("Invalid input\n\n");
+            return 1;
+        }
         printf("\nPlease enter number 2: ");
-        scanf("%lf", &num2);
+        //Validate taht a number was entered
+        if (scanf("%lf", &num2) != 1){
+            printf("Invalid input\n\n");
+            return 1;
+        }
     }
+    //If selection validation is invalid display an error
     else{
         printf("You have entered an invalid selection.\n\n");
-        return 0;
+        return 1;
     }
 
     //Consume the leftover newline character to allow for a pause before exiting at the end of the script
@@ -79,7 +87,7 @@ int main(void){
     }
 
     //Print the result
-    printf("Your answer is: %lf\n\n", result);
+    printf("Your answer is: %.2f\n\n", result);
 
     //Pause before exiting the program
     getchar();
