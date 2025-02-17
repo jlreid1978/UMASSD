523dd360439fdafed38c2f98a917b9d6ae47e0f9
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2025-02-17 11:56:23 -0500

    finished lab7 recursion

--- test.c
+++ test.c
@@ -1,17 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
 
 
-   char myString[100] = "1053 17.5 42 Smith";
-    float val1; 
-    int val2;
-    int val3; 
-    int val4;
+void Explore(int* allVals, int* pickedVals, bool* inUse, int totalPicked, int arraySize){
+    int i;
 
-    sscanf(myString, "%f %d %d %d", &val1, &val2, &val3, &val4); 
+    // Base case: All values are picked
+    if (totalPicked == arraySize) {
+        for (i = 0; i < totalPicked; ++i){
+            printf("%d ", pickedVals[i]);
+        }
+        printf("\n");
+    }
+    // Recursive case
+    else {
+        if (!inUse[i]) {
+            inUse[i]  = true;     
+               
+        for (i = 0; i < arraySize; ++i){
+            pickedVals[totalPicked] = allVals[i];
+            Explore(allVals, pickedVals, inUse, totalPicked + 1, arraySize);
+            inUse = false;
+        }
+    }
+    }
+}
 
-    printf("%f %d %d %d\n", val1, val2, val3, val4); 
+int main(void){
+    int arraySize;
+    int* valsToPick = NULL;
+    int* picks = NULL;
+    bool* inUse = NULL;
+    int i;
+
+    scanf("%d", &arraySize);
+
+    valsToPick = (int*) malloc(sizeof(int) * arraySize);
+    picks = (int*) malloc(sizeof(int) * arraySize);
+    inUse = (bool*) malloc(sizeof(int) * arraySize);
+
+    for (i = 0; i< arraySize; ++i) {
+        scanf("%d", &(valsToPick[i]));
+    }
+
+    printf("All possible ways to order an array of size %d:\n", arraySize);
+
+    Explore(valsToPick, picks, inUse, 0, arraySize);
+
+    free(valsToPick);
+    free(picks);
+    free(inUse);
 
     return 0;
+
 }
