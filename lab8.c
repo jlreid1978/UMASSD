2fd13c400e5ad7e0c217bcde810d60b5af039b57
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2025-02-17 13:44:38 -0500

    adding lab 8 and 9 shells

---
+++ lab8.c
@@ -0,0 +1,16 @@
+//Jesse Reid CIS-115 lab 8
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Create a program that performs the following tasks:
+// Define a macro arraySize of size n
+// Create two arrays of size n.
+// Pass these arrays to a function fillArrays that randomizes enough integers between 1-100 (inclusive) to fill both arrays.
+// Print both of these arrays.
+// Pass the filled arrays to a second function mergeArrays that creates a third array of size 2n.
+// Store the data from the first two arrays into the third array.
+// Print out your sorted array.
+
+int main(void) {
+    
+}
---
+++ lab9.c
@@ -0,0 +1,14 @@
+//Jesse Reid CIS-115 lab 8
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Complete the program to perform the following tasks, see attached .c source file.
+// Create three pointers of size 0, one each for integers, doubles, and characters
+// Prompt the user for which type of data they will enter (integer, double, character)
+// Reallocate the amount of memory that pointer has and add the new value to it
+// Print all three pointers and loop until the user chooses to exit
+// Be sure to upload all .c source files to the assignment posting on myCourses.
+
+int main(void) {
+    
+}
